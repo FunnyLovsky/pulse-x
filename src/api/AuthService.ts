e433a2ff07ffcc92242a16665051f03027edd615
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-01-25 20:33:31 +0300

    fix: authService

--- src/api/AuthService.ts
+++ src/api/AuthService.ts
@@ -3,7 +3,6 @@ import { IUser } from '../Models/IUser';
 export default class AuthService {
     static async getUser(): Promise<IUser[]> {
         const response = await fetch('./users.json');
-        console.log('authservice');
         return await response.json();
     }
 }
